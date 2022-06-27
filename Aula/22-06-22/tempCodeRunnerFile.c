8fa43b4de2fe742b77d4e39003280faff93b90a2
Joao Vargas <jotaguivargas@gmail.com>
2022-06-27 14:34:47 -0300

    27-06-22

--- Aula/22-06-22/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < array[i][0] + 1; j++)
-            {
-                printf("%d ", array[i][j]);
-            }
-            printf("\n");
-        }
